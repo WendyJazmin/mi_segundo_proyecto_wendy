d8688417837f9cd23829eeb307026da5ef622546
wendyjazmin <wendyjazmin1994@hotmail.com>
2022-06-18 17:22:20 -0500

    Mi segunda subida desde consola (Viernes-27-05-2022)

---
+++ src/main/java/com/uce/edu/demo/MiSegundoProyectoWendyApplication.java
@@ -0,0 +1,20 @@
+package com.uce.edu.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiSegundoProyectoWendyApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiSegundoProyectoWendyApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Creando el segundo proyecto en STS para Spring Framework");
+	}
+
+}
---
+++ src/main/java/com/uce/edu/demo/consultorio/CitaMedica.java
@@ -0,0 +1,23 @@
+package com.uce.edu.demo.consultorio;
+
+public class CitaMedica {
+
+	private Doctor doctor;
+	private Paciente paciente;
+	
+	//set y get
+	public Doctor getDoctor() {
+		return doctor;
+	}
+	public void setDoctor(Doctor doctor) {
+		this.doctor = doctor;
+	}
+	public Paciente getPaciente() {
+		return paciente;
+	}
+	public void setPaciente(Paciente paciente) {
+		this.paciente = paciente;
+	}
+	
+	
+}
